b84079c5ed499eaefe4c9477dbe31847de24a503
SalcedoEq <espequipamientobiomedico03@gmail.com>
2026-04-03 01:27:24 -0500

    index complete

--- src/main.js
+++ src/main.js
@@ -345,3 +345,53 @@ window.addEventListener("scroll",()=> {
       backButton.classList.remove("active");
     }
 })
+
+/*main content*/
+
+/*most frecuent*/
+const carruselChangeEqHear = document.querySelector(".see-equipamiento")
+const carruselChangeMobHear = document.querySelector(".see-mobiliario")
+const carruselChangeSeeEq = document.querySelector(".equipameinto-prev")
+const carruselChangeSeeMob = document.querySelector(".mobiliario-prev")
+const aUpdateEq = document.querySelector(".store-hear-Eq")
+const aUpdateMob = document.querySelector(".store-hear-Mob")
+
+carruselChangeEqHear.addEventListener("mouseenter", carruselChangeEq);
+carruselChangeMobHear.addEventListener("mouseenter", carruselChangeMob);
+
+function carruselChangeEq () {
+  carruselChangeSeeEq.classList.remove("inactive");
+  carruselChangeSeeMob.classList.add("inactive");
+  aUpdateEq.classList.add("active");
+  aUpdateMob.classList.remove("active");
+}
+function carruselChangeMob () {
+  carruselChangeSeeEq.classList.add("inactive");
+  carruselChangeSeeMob.classList.remove("inactive");
+  aUpdateEq.classList.remove("active");
+  aUpdateMob.classList.add("active");
+}
+
+/*most frecuent--------------------*/
+
+/*consultoria*/
+const cards = document.querySelectorAll(".consultoria-card");
+
+cards.forEach((card) => {
+    const front = card.querySelector(".consultoria-front");
+    const back = card.querySelector(".consultoria-back");
+    const icon = card.querySelector(".consultoria-icon");
+
+    card.addEventListener("mouseenter", () => {
+        front.classList.add("inactive");
+        back.classList.remove("inactive");
+        icon.classList.add("inactive");
+    });
+
+    card.addEventListener("mouseleave", () => {
+        front.classList.remove("inactive");
+        back.classList.add("inactive");
+        icon.classList.remove("inactive");
+    });
+});
+/*consultoria---------------------*/
